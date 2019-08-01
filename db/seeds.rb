422b5bb6e5f994f35eb91ab28cb4f08e90a17a78
ekwayv8 <yvon.ekwalla@gmail.com>
2019-08-01 17:52:22 +0200

    Finalisation du 1er exo avec Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Création de 2 fake users
+#User.create(first_name: "jean", email:"jean@jean.jean")
+#ser.create(first_name: "paul", email:"paul@paul.paul")
+#puts "Deux utilisateurs ont été créés"
+
+
+#Permet de créer une liste de faux objets avec la gem Faker
+require 'faker'
+
+#Permet de supprimer les informations précédements stockées afin de disposer d'un tableau MAJ
+City.destroy_all
+Doctor.destroy_all
+Patient.destroy_all
+Appointment.destroy_all
+Specialty.destroy_all
+JoinDocSpec.destroy_all
+
+#Boucle permettant d'afficher les "fake" objets
+100.times do
+
+  city_name = City.create!(name: Faker::Address.city)
+  doctor_rand = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, zip_code: Faker::Name.last_name, city: city_name )
+  patient_rand = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city: city_name)
+  appointment_rand = Appointment.create!(date: Faker::Date.forward,doctor:doctor_rand,patient: patient_rand, city: city_name)
+  specialty_rand = Specialty.create!(job: Faker::Educator.subject)
+  join_doc_spec = JoinDocSpec.create!(doctor:doctor_rand,specialty: specialty_rand)
+
+end
